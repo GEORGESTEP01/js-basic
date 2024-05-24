7f6ad71b2ba0fe74f488340eb6daa7986c53b2d9
George <geo.che66@gmail.com>
2024-05-24 09:39:04 +0530

    new configuration

---
+++ area.js
@@ -0,0 +1,17 @@
+// alert('welcome to js learning');
+
+console.log("greatings to all");
+console.log(123456789);
+console.table({
+    number:1, color:'green'
+});
+console.table([1,2,4,5]);
+
+console.error('dont be lasy ');
+
+console.time('time starts');
+for (c=0;c<10111;c++){
+console.log(c);
+}
+console.timeEnd('time starts');
+
